f095ca8b7fe5d435084020a68c401830a2be7743
prachi1505 <85737452+prachi1505@users.noreply.github.com>
2021-06-24 15:44:21 +0530

    traveling in a ship

---
+++ Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -0,0 +1,23 @@
+
+function preload(){
+seaImg=loadImage("sea.png");
+shipImg=loadAnimation("ship-1.png,ship-2.png,ship-3.png,ship-4.png");
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ sea.addImage(seaImg);
+
+if(sea.x<0){
+sea.x=sea.width/2;
+
+}
+
+
+}
